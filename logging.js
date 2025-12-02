629e6237aa75fbd6f2c439fc9500817b3f12f7d2
walkingcuber <jses1080058@smail.ilc.edu.tw>
2025-12-02 22:33:19 +0800

    Update logging.js

--- logging.js
+++ logging.js
@@ -1,20 +1,15 @@
 import { auth, signInWithEmailAndPassword } from './Firebase.Js';
 
-const loginBtn = document.getElementById('login-btn');
-const usernameInput = document.getElementById('username-input');
-const passwordInput = document.getElementById('password-input');
-const loginError = document.getElementById('login-error');
-
-loginBtn.addEventListener('click', () => {
-    const email = usernameInput.value;
-    const password = passwordInput.value;
+document.getElementById("login-btn").addEventListener("click", function() {
+    const email = document.getElementById("username-input").value;
+    const password = document.getElementById("password-input").value;
 
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // 登入成功，跳轉到 Dashboard
-            window.location.href = 'Dashboard.html';
+            // 登入成功
+            window.location.href = "Dashboard.html";
         })
         .catch((error) => {
-            loginError.textContent = error.message;
+            document.getElementById("login-error").textContent = error.message;
         });
 });
