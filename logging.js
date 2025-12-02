a6086ad9b67136996add086af56fda83d94345f4
walkingcuber <jses1080058@smail.ilc.edu.tw>
2025-12-02 22:35:40 +0800

    Update logging.js

--- logging.js
+++ logging.js
@@ -1,15 +1,14 @@
-import { auth, signInWithEmailAndPassword } from './Firebase.Js';
-
 document.getElementById("login-btn").addEventListener("click", function() {
     const email = document.getElementById("username-input").value;
     const password = document.getElementById("password-input").value;
 
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // 登入成功
+    auth.signInWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            // 登入成功跳轉 Dashboard
             window.location.href = "Dashboard.html";
         })
-        .catch((error) => {
+        .catch(error => {
+            // 顯示錯誤訊息
             document.getElementById("login-error").textContent = error.message;
         });
 });
